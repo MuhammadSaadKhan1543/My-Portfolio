a11b0eea1afdf481521b29b33ec4c74488f6a552
MuhammadSaadKhan1543 <157738156+MuhammadSaadKhan1543@users.noreply.github.com>
2026-05-08 03:02:14 +0500

    fixed probably

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../images/logo.png";
+import logo from "../assets/logo.png";
 import { Lock , Menu , X} from 'lucide-react';
 import { NavLink, Link } from 'react-router-dom';
 import { useState } from "react";
